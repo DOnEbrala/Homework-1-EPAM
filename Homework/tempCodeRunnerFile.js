0a69b044ceb9c291ffa08aaec1eade4ff88c4746
DOnEbrala <ebraebra13@gmail.com>
2022-09-18 23:20:01 +0400

    new homework-5

--- Homework/tempCodeRunnerFile.js
+++ Homework/tempCodeRunnerFile.js
@@ -1,33 +1,6 @@
-// Create 2 objects: animal and cat, add move property to animal object, cat object must inherit move property from object animal
+const letter1 = 'ahb acb aeb aeeb adcb axeb';
 
 
-class Animal {
-    constructor(name, ability){
-        this.name = name;
-        this.ability = ability;
-    }
+console.log(letter1.match(/a.b/g));
 
 
-abilities() {
-    console.log(`this is a ${this.name} ${this.ability}`);
-}
-
-}
-
-
-
-class Cat extends Animal {
-    
-    constructor(name, ability){
-        super(name, ability);
-    }
-    skill(){
-        console.log(` he can ${this.ability}`)
-    }
-
-}
-
-const dogg = new Cat("cat", "swim");
-
-dogg.abilities();
-dogg.skill();
