81d14798469e7e7b5c7f2ee14966649151cc1bca
DOnEbrala <ebraebra13@gmail.com>
2022-09-29 18:52:21 +0400

    finally fix

--- Homework/Homework_6.js
+++ Homework/Homework_6.js
@@ -1,14 +1,29 @@
 //In a try catch construction, wrap the code: console.log (a), let a = 3. And display an error - ‘let must be declared’ before use. When running 1/0, the error 'cannot be divided by zero'
 
 
+
+//1
 try {
     console.log(a)
     let a = 3;
     
 } catch (err) {
     alert( 'let must be declared' );
-    let b = 1/0;
 
-} finally{
-    alert("cannot be divided by zero'")
+}
+
+
+//2
+
+try {debugger
+    let b = 1/0;
+    if (b == Infinity ){
+        throw new Error("cannot divide by zero")
+    }
+    console.log(b);
+
+} catch (err){
+
+    console.log(err);
+
 }
--- Homework/tempCodeRunnerFile.js
+++ Homework/tempCodeRunnerFile.js
@@ -1,11 +1,12 @@
 try {
-    console.log(a)
-    let a = 3;
-    
-} catch (err) {
-    alert( 'let must be declared' );
     let b = 1/0;
+    if (b == Infinity ){
+        throw new Error("cannot divide by zero")
+    }
+    console.log(b);
 
-} finally{
-    alert("cannot be divided by zero'")
+} catch (err){
+
+    console.log(err);
+
 }
