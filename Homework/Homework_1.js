a71f2cf9fd3a9ce0de5950f1541275d75258899e
Nika <ebraebra13@gmail.com>
2022-10-30 22:58:25 +0400

    fix homework-1

--- Homework/Homework_1.js
+++ Homework/Homework_1.js
@@ -1,11 +1,11 @@
 let fname = "Nika";
-let Lname = true;
+let lname = true;
 
-const x = fname + " " + Lname;
+const x = fname + " " + lname;
 
 console.log(x);
 
-// A
+
 
 let numb = 5;
 let str = "6";
@@ -14,16 +14,17 @@ const answer = numb + str;
 
 console.log(answer);
 
-//A-1
 
-let born = "2000";
-let today = 2022;
 
-const age = today - born;
+let bornYear = "2000";
+let presentYear = 2022;
+
+const age = presentYear - bornYear;
 
 console.log(age);
 
-// A-2
+
+
 
 let g = 25;
 let w = true;
@@ -32,7 +33,7 @@ const u = g + w;
 
 console.log(u);
 
-// B
+
 
 let a = "10";
 let b = false;
@@ -41,7 +42,7 @@ const y = a * b;
 
 console.log(y);
 
-// B-2
+
 
 let lobiani = 5;
 let khachapuri = "10";
@@ -50,7 +51,7 @@ const Supra = lobiani * khachapuri;
 
 Supra === 50 ? console.log("true") : console.log("false");
 
-// B-2-1
+
 
 let apple = "two";
 let banana = 11;
@@ -59,16 +60,16 @@ const basket = apple * banana; //console.log(Basket);
 
 basket === 22 ? console.log("true") : console.log("false");
 
-// B-3
 
-let razer = true;
-let hyperx = 33;
 
-const r = razer * hyperx;
+let razerX = true;
+let hyperX = 33;
+
+const r = razerX * hyperX;
 
 console.log(r);
 
-// C
+
 
 let khinkali = "meat";
 let pelmeni = false;
@@ -77,7 +78,7 @@ const food = khinkali / pelmeni;
 
 console.log(food);
 
-//C-1-1
+
 
 let keyboard = "10";
 let mice = true;
@@ -86,7 +87,7 @@ const peripherals = keyboard / mice;
 
 console.log(peripherals);
 
-//C-2
+
 
 let lg = "Washmachine";
 let bosh = 55;
@@ -95,7 +96,7 @@ const tehc = lg / bosh;
 
 console.log(tehc);
 
-//C-2-1
+
 
 let joystick = "23";
 let wheel = 32;
@@ -104,7 +105,7 @@ const game = joystick / wheel;
 
 console.log(game);
 
-//C-3
+
 
 let masterChief = 117;
 let cortana = false;
@@ -113,41 +114,31 @@ const halo = masterChief / cortana;
 
 console.log(halo);
 
-//D
+
 
 const inputYear = "1991";
 
 console.log(Number(inputYear) + 18);
 
-//D-1-1
+
 
 const key = 20;
 
 console.log(String(key) - "10");
 
-//or
-
 console.log(String(key) + 10);
-
-//D-1-2
 
 const myName = "Nika";
 
 console.log(Number("myName"));
 
-//D-1-3
-
 console.log(Boolean(25));
 
 console.log(Boolean(0));
 
-//D-1-4
-
 console.log(Number(true));
 
 console.log(Number(false));
-
-//D-1-5
 
 console.log(Boolean("Converted Boolean"));
 //or
@@ -158,12 +149,14 @@ console.log(String(false));
 console.log(String(true));
 //or
 
+
+
 const me = true;
 const you = Boolean("man");
 
 console.log(me + you);
 
-//D-1-6
+
 
 const luka = String(25);
 const liza = Number("23");
--- Homework/tempCodeRunnerFile.js
+++ Homework/tempCodeRunnerFile.js
@@ -1,22 +1,14 @@
-function Warehouse() {
-  let apples = 0;
-  let pears = 0;
+const Car = {
+  car: "Porsche",
+  color: "Black",
+  HorsePower() {
+    let Power = 562;
+    this.HorsePower = Power;
+  },
+};
 
-  return function () {
-    console.log(
-      "In Warehouse, We accepted " +
-        apples +
-        " apples " +
-        "and " +
-        pears +
-        " pears "
-    );
-    return apples++, pears++;
-  };
-}
+Car.color = "green";
 
-const Count = Warehouse();
-console.log(Count());
-console.log(Count());
-console.log(Count());
-console.log(Count());
+Car.HorsePower();
+
+console.log(Car);
